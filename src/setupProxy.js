d5e7b213132f484381d306d995798bdbc4c1b1f4
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2022-12-02 10:49:00 +0530

    f22

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://bookmyshow-project-backend.vercel.app/',
+      changeOrigin: true,
+    })
+  );
+};
