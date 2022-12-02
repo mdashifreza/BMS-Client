e8ba9bdce255c2ca7d751d109a968e9147fa19d4
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2022-12-02 16:29:24 +0530

    f24

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/',
+    ['/api*'],
     createProxyMiddleware({
       target: 'https://bookmyshow-project-backend.vercel.app/',
       changeOrigin: true,
